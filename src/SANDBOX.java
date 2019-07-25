1b832cd4e8929dd9d9881a0461e400b320875632
Steven <steven.turner12x@gmail.com>
2019-07-25 15:47:27 -0500

    all

---
+++ src/SANDBOX.java
@@ -0,0 +1,9 @@
+public class SANDBOX {
+    public static void main(String[] args) {
+        if (100 < 1000) {
+            System.out.println("jes, mayne");
+        } else {
+            System.out.printf("nah bruddah");
+        }
+    }
+}
