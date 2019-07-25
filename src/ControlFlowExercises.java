919c740ff70ae78b699e49558ae3c815794e6da5
Steven <steven.turner12x@gmail.com>
2019-07-25 16:59:27 -0500

    power table

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,18 +1,26 @@
-
+import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (int i = 1;i <= 50;i += 1) {
-            if (i % 15 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else {
-                System.out.println(i);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int userInput = scanner.nextInt();
+        System.out.print("Here is your Table!\n");
+        System.out.print(" number     | squared     | cubed \n");
+        for (int i = 1; i <= userInput; i+=1) {
+            System.out.format("     %s      |     %s      |     %s     \n", i, (i*i), (i*i*i));
+        }
+        System.out.print("Would you like to continue? yes/no?");
+        String yesOrNo = scanner.next();
+        if (yesOrNo == "yes") {
+            System.out.print(" number     | squared     | cubed \n");
+            for (int y = userInput; y <= userInput*2; y+=1) {
+                System.out.format("     %n      |     %n      |     %n     \n", y, (y*y), (y*y*y));
             }
+        } else if (yesOrNo == "no") {
+            System.out.println("All done");
         }
     }
+
 }
 ;
