e0bc2054919a710de76524693d35ae76dba3a35b
Steven <steven.turner12x@gmail.com>
2019-07-25 16:26:31 -0500

    fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,8 +2,16 @@
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (long i = 2; i <= 1000000; i*=i) {
-            System.out.println(i);
+        for (int i = 1;i <= 50;i += 1) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }
