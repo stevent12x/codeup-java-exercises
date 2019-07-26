384e41c42f0e113b873201837510fbe9ea1ae965
Steven Turner <steven.turner12x@gmail.com>
2019-07-26 16:32:30 -0500

    stringExercises - pt1 finished

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,7 +1,8 @@
 public class StringExercise {
 
     public static void main(String[] args) {
-        String message = "We don't need no education \nWe don't need no thought control";
-        System.out.print(message);
+        String message = "We don't need no education \nWe don't need no thought control Check \"this\" out!, \"s inside of \"s! In windows, the main drive is usually C:\\ I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(message.substring(0, message.indexOf("Check")) + "\n\n" + (message.substring(message.indexOf("Check") , message.lastIndexOf("In"))) + "\n\n" + (message.substring(message.indexOf("In"), (message.lastIndexOf("I"))) + "\n\n" + message.substring(message.lastIndexOf("I"))));
     }
 }
