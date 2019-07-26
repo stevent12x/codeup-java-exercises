f9616a9b546d9c204919aa9d190f7f062d856299
Steven Turner <steven.turner12x@gmail.com>
2019-07-26 15:16:08 -0500

    strings ex 1

---
+++ src/StringExercise.java
@@ -0,0 +1,7 @@
+public class StringExercise {
+
+    public static void main(String[] args) {
+        String message = "We don't need no education \nWe don't need no thought control";
+        System.out.print(message);
+    }
+}
