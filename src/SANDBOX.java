3807a13d9dbf3585fd18f25b1fccf32ed871f331
Steven Turner <steven.turner12x@gmail.com>
2019-07-29 12:59:35 -0500

    sandbox

--- src/SANDBOX.java
+++ src/SANDBOX.java
@@ -1,9 +1,28 @@
 public class SANDBOX {
     public static void main(String[] args) {
-        if (100 < 1000) {
+        int i = 3000;
+
+        if (i < 10) {
             System.out.println("jes, mayne");
-        } else {
-            System.out.printf("nah bruddah");
         }
+        if (i > 100) {
+            System.out.println("wassup");
+        } else {
+            System.out.println("nah bruddah");
+        }
+
     }
+
+    public static int multiply(int a, int b) {
+        if (a <= 0) {
+            System.out.println("last time a =" + a + " b=" + b);
+            return 0;
+        } else {
+            System.out.println("a=" + a + " b=" + b);
+            return b + multiply(a - 1, b);
+
+        }
+
+    }
+
 }
