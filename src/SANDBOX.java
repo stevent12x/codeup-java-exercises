f6529bb8bfb5a4956bbc1e94c32b1a3b5a9e8cc8
Steven Turner <steven.turner12x@gmail.com>
2019-07-31 15:02:19 -0500

    java arrays sandbox

--- src/SANDBOX.java
+++ src/SANDBOX.java
@@ -1,8 +1,14 @@
 public class SANDBOX {
     public static void main(String[] args) {
-        int x = 5 * 4 % 3;
-        System.out.println(x);
-        String y = "twenty";
-        System.out.println(y + x);
+
+    String[] beatles = new String[4];
+    beatles[0] = "John";
+    beatles[1] = "George";
+    beatles[2] = "Ringo";
+    beatles[3] = "Paul";
+
+    for (String beatle : beatles) {
+        System.out.println(beatle);
+    }
     }
 }
