09dddf05e03b9800e2786a42c90b8c7c9924c50c
Steven Turner <steven.turner12x@gmail.com>
2019-08-05 13:55:13 -0500

    working on #3

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,12 +1,19 @@
 package grades;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class GradesApplication {
     public static void main(String[] args) {
 
         // New Hash Map
         HashMap<String, Student> students = new HashMap<>();
+
+        //New Scanner
+
+        Scanner scanner = new Scanner(System.in);
 
         // New Students
         Student patrick = new Student("Starfish", "Patrick", "psHDFW");
@@ -17,10 +24,13 @@ public class GradesApplication {
         // Student Grades
         patrick.addGrade(24);patrick.addGrade(51)
         ;patrick.addGrade(63);patrick.addGrade(30);
+
         sandy.addGrade(101);sandy.addGrade(99)
         ;sandy.addGrade(105);sandy.addGrade(100);
+
         steven.addGrade(89);steven.addGrade(93)
         ;steven.addGrade(75);steven.addGrade(96);
+
         katie.addGrade(92);katie.addGrade(97)
         ;katie.addGrade(100);katie.addGrade(96);
 
@@ -30,6 +40,21 @@ public class GradesApplication {
         students.put(steven.getGitHubUN(), steven);
         students.put(katie.getGitHubUN(), katie);
 
+        // Print github usernames
 
-   }
+
+        System.out.println("gitHub usernames = " + students.keySet());
+        System.out.println("Which user would you like to know more about?");
+
+        String userInput = scanner.nextLine();
+
+        for (String student: students.keySet()) {
+            if (userInput.equalsIgnoreCase(student)) {
+                System.out.println(students.get(student).getName() + " has a GPA of : " + students.get(student).getGradeAverage());
+                break;
+            } else if (userInput != student){
+                System.out.printf("");
+            }
+        }
+    }
 }
