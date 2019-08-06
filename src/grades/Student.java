7efbb0af1d6bf4cbac0b39eaf3747ff4e3857105
Steven Turner <steven.turner12x@gmail.com>
2019-08-06 09:20:29 -0500

    8/6 morning commit

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -12,7 +12,7 @@ public class Student {
     private String lastName;
     private String gitHubUN;
     private ArrayList<Integer> grades;
-    public Scanner scanner = new Scanner(System.in);
+
 
     // Student Constructor
     public Student(String lastName, String firstName, String gitHubUN) {
@@ -23,11 +23,9 @@ public class Student {
     }
 
     // Student Methods
-    public String getName(){
-        return firstName + " " + lastName;
-    }
+    public String getName(){return this.firstName + " " + this.lastName;}
     public void addGrade(int grade) {
-        grades.add(grade);
+        this.grades.add(grade);
     }
     public double getGradeAverage() {
         double gradeAverage = 0;
@@ -37,24 +35,10 @@ public class Student {
         return (gradeAverage/grades.size());
     }
     public String getGitHubUN() {
-        return gitHubUN;
+        return this.gitHubUN;
     }
     public String getGrades(){
-        return grades.toString();
+        return this.grades.toString();
     }
 
-//
-//    public static void main(String[] args) {
-//        Student patrick = new Student("Starfish", "Patrick", "psHDFW");
-//        Student sandy = new Student("Cheeks", "Samantha", "CheekyTime29");
-//        Student steven = new Student("Turner", "Steven", "stevent12x");
-//        Student katie = new Student("Wise", "Katelin", "ClaireBear07");
-//
-//        patrick.addGrade(24);patrick.addGrade(51);patrick.addGrade(63);patrick.addGrade(30);
-//        sandy.addGrade(101);sandy.addGrade(99);sandy.addGrade(105);sandy.addGrade(100);
-//        steven.addGrade(89);steven.addGrade(93);steven.addGrade(75);steven.addGrade(96);
-//        katie.addGrade(92);katie.addGrade(97);katie.addGrade(100);katie.addGrade(96);
-//
-//        System.out.println("katie.getGrades() = " + katie.getGrades());
-    }
-//}
+}
