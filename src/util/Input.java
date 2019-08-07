dd9a08cacc044214893386b3e7e408a112bb6264
Steven Turner <steven.turner12x@gmail.com>
2019-08-07 13:12:43 -0500

    double down

--- src/util/Input.java
+++ src/util/Input.java
@@ -2,22 +2,19 @@ package util;
 import java.util.Scanner;
 
 public class Input {
+
     public Scanner scanner;
-    public Input() {
-        scanner = new Scanner(System.in);
-    }
+    public Input() {this.scanner = new Scanner(System.in); }
 
     public String getString() {
-        Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string");
-        String userInput = sc.nextLine();
+        String userInput = this.scanner.nextLine();
         return userInput;
     }
 
     public boolean yesOrNo() {
-        Scanner sc = new Scanner(System.in);
         System.out.println("Enter yes or no");
-        String userInput = sc.nextLine();
+        String userInput = this.scanner.nextLine();
         do {
             if (userInput.toUpperCase().contains("Y")) {
                 return true;
@@ -27,39 +24,61 @@ public class Input {
         } while (true);
     }
 
-    public static int getInt(int min, int max){
-        int userInput = 0;
-        Scanner sc = new Scanner(System.in);
-        try {
-            System.out.printf("Enter a number between %s and %s\n", min, max);
-            do {
-                userInput = Integer.valueOf(sc.nextLine());
-                if (userInput >= min && userInput <= max)
-                    return userInput;
-            } while (true);
-        } catch (NumberFormatException ex) {
-            System.out.println("Number Format Exception - Must Enter Int!");
-        } finally {
-            System.out.println("Keep it up Buttercup!");
-        } return userInput;
+    public int getInt(int min, int max) {
+        do {
+            System.out.printf("Enter a number between %s and %s \n", min, max);
+            String userInput = this.scanner.nextLine();
+            int realAnswer;
+            try {
+                realAnswer = Integer.valueOf(userInput);
+            } catch (NumberFormatException e) {
+                continue;
+            }
+            if (realAnswer >= min && realAnswer <= max) {
+                return realAnswer;
+            }
+        } while (true);
+    }
+    public double isDouble() {
+        do {
+            System.out.println("Enter a double");
+            String userInput = this.scanner.nextLine();
+            double realAnswer;
+            try {
+                realAnswer = Double.valueOf(userInput);
+            } catch (NumberFormatException e) {
+                continue;
+            }
+            return realAnswer;
+        } while (true);
     }
 
-    public static double getDouble() {
-        double userInput = 0;
-        Scanner sc = new Scanner(System.in);
-        try {
-            System.out.println("Enter a double");
-             userInput = Double.valueOf(sc.nextLine());
-        } catch (NumberFormatException ex) {
-            System.out.println("Number Format Exception! - Must enter Double!");
-        } finally {
-            System.out.println("You're doing great and you're super good looking to boot!");
-        } return userInput;
+    public double getDouble(double min, double max) {
+        do {
+            System.out.printf("Enter a double between %s and %s \n", min, max);
+            String userInput = this.scanner.nextLine();
+            double realAnswer;
+            try {
+                realAnswer = Double.valueOf(userInput);
+            } catch (NumberFormatException e) {
+                continue;
+            }
+            if (realAnswer >= min && realAnswer <= max) {
+                return realAnswer;
+            }
+        } while (true);
     }
 
     public static void main(String[] args) {
-        System.out.println("You're Double is: " + getDouble());
-        System.out.println("You're Integer is " + getInt(1, 20));
+        Input input = new Input();
+        input.getDouble(3,100);
+        input.isDouble();
+
     }
 }
+
+
+
+
+
 
---
+++ src/util/InputTest.java
@@ -0,0 +1,7 @@
+package util;
+
+public class InputTest {
+    static void main(String[] args) {
+
+    }
+}
