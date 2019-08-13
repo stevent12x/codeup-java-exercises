d4947d886e85c14ea2ff4999fe7379813b11d99a
Steven Turner <steven.turner12x@gmail.com>
2019-08-13 12:39:00 -0500

    first mysql push

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+
+
+CREATE TABLE albums (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+artist VARCHAR(100),
+name VARCHAR(100),
+release_date DATE,
+sales INT,
+genre VARCHAR(100),
+PRIMARY KEY (id)
+)
