6fdaab7c2bb077178058d0fc05a286e69bd5ddbe
Steven Turner <steven.turner12x@gmail.com>
2019-08-14 11:48:02 -0500

    update exercie

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,8 +1,8 @@
 USE codeup_test_db;
 
-SELECT name from albums where artist="Pink Floyd";
+SELECT name FROM albums where artist="Pink Floyd";
 select release_date from albums where name="Sgt. Pepper's Lonely Hearts Club Band";
 select genre from albums where name="Nevermind";
-select artist,name from albums where release_date >= 1990 AND release_date <= 1999;
-select artist,name from albums where sales < 20.0;
-select artist, name from albums where genre = "Rock";
+select name from albums where release_date >= 1990 AND release_date <= 1999;
+select name from albums where sales < 20.0;
+select name from albums where genre = "Rock";
---
+++ update_exercises.sql
@@ -0,0 +1,16 @@
+USE codeup_test_db;
+
+SELECT * FROM albums;
+UPDATE albums SET sales = sales * 10;
+SELECT * FROM albums;
+
+SELECT name FROM albums
+WHERE release_date < 1980;
+UPDATE albums SET release_date = release_date-100
+WHERE release_date < 1980;
+SELECT * FROM albums;
+
+SELECT name FROM albums WHERE artist = "Michael Jackson";
+UPDATE albums SET artist = "Peter Jackson"
+WHERE artist = "Michael Jackson";
+SELECT * FROM albums WHERE artist = "Peter Jackson";
