ed65e9a3b4c29f6f6efa860f02ebda4d38540d6e
Steven Turner <steven.turner12x@gmail.com>
2019-08-14 14:00:03 -0500

    exit

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,8 +1,14 @@
 USE codeup_test_db;
 
+SELECT 'The name of all albums by Pink Floyd' as '';
 SELECT name FROM albums where artist='Pink Floyd';
+
+SELECT 'The release date of Sgt. Pepper"s' as '';
 select release_date from albums where name='Sgt. Pepper"s Lonely Hearts Club Band';
+
+SELECT 'The genre of Nirvana"s Nevermind' as '';
 select genre from albums where name='Nevermind';
+
 select name from albums where release_date >= 1990 AND release_date <= 1999;
 select name from albums where sales < 20.0;
 select name from albums where genre = 'Rock';
