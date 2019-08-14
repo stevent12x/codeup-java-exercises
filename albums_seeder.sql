d00db476c83c19147aaf318aa8a844dc380581a3
Steven Turner <steven.turner12x@gmail.com>
2019-08-14 10:27:50 -0500

    albums exercise and 8/14 am commit

---
+++ albums_seeder.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+
+insert into albums
+    (artist , name , release_date , sales , genre)
+    VALUES
+    ("Eagles" , "Their Greatest Hits (1971 - 1975)" , 1976 , 41.2 , "Country Rock , Soft Rock , Folk Rock"),
+    ("Michael Jackson" , "Thriller" , 1982 , 47.3 , "Pop , Rock , R&B"),
+    ("Eagles" , "Hotel California" , 1976 , 31.5 , "Soft Rock");
+
+
