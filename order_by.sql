8f9c1b1e320a4c056c3e764e1f58efb509bf1adc
Steven Turner <steven.turner12x@gmail.com>
2019-08-15 11:35:53 -0500

    truly finished where / moving on to order_by

---
+++ order_by.sql
@@ -0,0 +1,13 @@
+use employees;
+
+# -- Show the first 20 employees
+# SELECT *
+# FROM employees
+# ORDER BY emp_no ASC
+# limit 20 offset 0;
+#
+# -- Show the 2nd batch of 20 employees
+# SELECT *
+# FROM employees
+# ORDER BY emp_no ASC
+# LIMIT 20 offset 20;
