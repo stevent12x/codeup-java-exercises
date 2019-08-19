db735c2720bf821c07dcfb0edc3a381c2d9bc970
Steven Turner <steven.turner12x@gmail.com>
2019-08-19 09:52:20 -0500

    8/19 morning commit

--- join_exercises.sql
+++ join_exercises.sql
@@ -22,20 +22,22 @@ ORDER BY Department ASC;
 
 -- Distribution of Titles in Customer Service Department --
 SELECT t.title, count(t.title)
-from titles t
     JOIN dept_emp de on de.emp_no = t.emp_no
     JOIN departments d on de.dept_no = d.dept_no
 WHERE t.to_date > curdate()
-and d.dept_name = 'Customer Service'
+and d.dept_name = 'Customer Service'`123`   1 frews z
 GROUP BY t.title;
 
 
 -- Current salaries for all managers --
 SELECT d.dept_name as 'Department', CONCAT(e.first_name, ' ', e.last_name) as 'Manager', s.salary as 'Salary'
 FROM departments as d
-    JOIN dept_manager dm on d.dept_no = dm.dept_no
-    JOIN salaries s on dm.emp_no = s.emp_no
-    JOIN employees e on dm.emp_no = e.emp_no
+         JOIN dept_manager dm on d.dept_no = dm.dept_no
+         JOIN salaries s on dm.emp_no = s.emp_no
+         JOIN employees e on dm.emp_no = e.emp_no
 WHERE dm.to_date = '9999-01-01' and s.to_date = '9999-01-01'
 ORDER BY Salary DESC ;
+
+SELECT * from employees;
+
 
